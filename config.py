238251219fc7bf707daba424694d759531d83dd3
Valeriy Svystunov <65670141+v-v-svystunov@users.noreply.github.com>
2022-07-12 18:42:13 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+def get_db_host():
+    return "localhost"
+
+def get_db_name():
+    return ""
+
+def get_db_user():
+    return ""
+
+def get_db_pass():
+    return ""
+
+def get_defined_method_list():
+    return ['GET', 'POST', 'PUT', 'PATCH']
